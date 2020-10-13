c329fd39ce7cddb5bf793bc04f63a04387eaa7c9
YankoB <yankoo_bobev@abv.bg>
2020-10-13 22:57:09 +0000

    ubung3

---
+++ src/exercise3/NumberSearch.java
@@ -0,0 +1,18 @@
+package exercise3;
+
+public class NumberSearch {
+
+  public static void main(String[] args){
+    int anzahl = 0;
+   for(int a = 100; a<=1000; a++){
+     if(a%5==0 && a%6==0){
+       System.out.print(a + " ");
+       anzahl++;
+       if(anzahl==10){
+         System.out.println();
+         anzahl=0;
+       }
+     }
+   }
+  }
+}
