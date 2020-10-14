139aa63f30bccb89f81501ae74ab2a548d5783fc
YankoB <yankoo_bobev@abv.bg>
2020-10-14 15:18:49 +0000

    ubung3

---
+++ src/exercise3/AngleCalculation.java
@@ -0,0 +1,29 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class AngleCalculation{
+
+  public static void main(String[] args){
+  Scanner input = new Scanner(System.in);
+
+  double a = input.nextDouble();
+  char b = input.next().charAt(0);
+  while(b=='r'||b=='d'){
+    if(b=='r'){
+      double Grad = 180*a/Math.PI;
+      System.out.println("Angle: " + Grad + "d");
+      a= input.nextDouble();
+      b= input.next().charAt(0);
+    }
+    else if(b=='d'){
+      double Rad = a*Math.PI/180;
+      System.out.println("Angel: " + Rad + "r");
+      a= input.nextDouble();
+      b= input.next().charAt(0);
+    }else{
+      break;
+    }
+  }
+  }
+}
--- src/exercise3/NumberSearch.java
+++ src/exercise3/NumberSearch.java
@@ -6,9 +6,12 @@ public class NumberSearch {
     int anzahl = 0;
    for(int a = 100; a<=1000; a++){
      if(a%5==0 && a%6==0){
-       if(anzahl<10){
+       if(anzahl<9){
          System.out.print(a + " ");
          anzahl++;
+       }else if(anzahl==9){
+         anzahl++;
+         System.out.print(a);
        } if(anzahl==10){
          System.out.print("\n");
          anzahl=0;
